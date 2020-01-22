18eef019ab8e18aec34d061ffdf2767e411e9f04
goobway <calistagreenway@gmail.com>
2020-01-22 00:26:58 -0500

    small changes to css

--- thumbnails/app.js
+++
@@ -1,9 +0,0 @@
-const fullSizeImgs = document.getElementsByClassName('slides');
-
-let currentImage = 0;
-
-function reset() {
-    for (let img of fullSizeImgs) {
-        img.classList.remove('show');
-    }
-}
