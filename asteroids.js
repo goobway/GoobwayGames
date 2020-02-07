e5b3ec3be07333c7a86e1ebaf08bf0ac7ac2de88
goobway <calistagreenway@gmail.com>
2020-02-07 12:49:36 -0500

    Updating Canvas for Asteroids game

--- asteroids.js
+++ asteroids.js
@@ -1,8 +1,25 @@
 var c = document.getElementById("canvas");
+c.height = 1000;
+c.width = 1300;
 var ctx = c.getContext("2d");
-ctx.beginPath();
-// The arc() method creates an arc/curve
-// To create a circle with arc(): Set start angle to 0 and end angle to 2*Math.PI
-// JS syntax: content.arc(x,y,r,sAngle,eAngle,counterclockwise)
-ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-ctx.stroke();
+var x = 10,
+    y = 100;
+ctx.fillStyle = "black";
+ctx.fillRect(700, 100, 100, 100);
+
+function draw() {
+    ctx.beginPath();
+    // The arc() method creates an arc/curve
+    // To create a circle with arc(): Set start angle to 0 and end angle to 2*Math.PI
+    // JS syntax: content.arc(x,y,r,sAngle,eAngle,counterclockwise)
+    ctx.arc(x, y, 20, 0, 2 * Math.PI);
+    ctx.fillStyle = 'rgba(250,0,0,0.4)';
+    ctx.fill();
+
+    x += 2;
+    ctx.fillStyle = 'rgba(34,45,23,0.4)';
+    ctx.fillRect(0, 0, c.width, c.height);
+    // requestAnimationFrame(callback) calls a function to update an animation before the next repaint
+    requestAnimationFrame(draw);
+}
+draw();
